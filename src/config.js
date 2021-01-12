3302c38ba29469221410d911c36d23e422ccfe88
StefanVinica <stefanikolovski996@gmail.com>
2021-01-12 13:41:39 -0800

    sd

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgres://xqlgplvuuwjyet:5d1f5c21a6614abd6a0bada034850ca18daeb829abdedf2de0808fbd185817d4@ec2-54-146-4-66.compute-1.amazonaws.com:5432/dabsmrd4pghsli",
+    "postgres://aymhcbvnjrwdjb:0edb0f23a9ee2992c63c3c3549b3fc40a17617767de4b8a30018fbb785e75cf1@ec2-54-146-91-153.compute-1.amazonaws.com:5432/d5a5epjlvg1ol6",
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
