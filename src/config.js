6c1199a76dc4950edf19b23b12ed53f2ea77fa98
StefanVinica <stefanikolovski996@gmail.com>
2021-01-12 12:42:36 -0800

    db

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgres://xqlgplvuuwjyet@ec2-54-146-4-66.compute-1.amazonaws.com",
+    "postgres://xqlgplvuuwjyet@ec2-54-146-4-66.compute-1.amazonaws.com/dabsmrd4pghsli",
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
